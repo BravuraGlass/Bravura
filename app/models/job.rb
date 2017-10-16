4f0b0d3cb1af1d1aaaac2ac65dabc1f079b62727
Aditya Agustyana <aditya@prawirasoft.com>
2017-10-16 23:31:03 +0800

    lined up the rows

--- app/models/job.rb
+++ app/models/job.rb
@@ -18,33 +18,45 @@ class Job < ApplicationRecord
     self.fabrication_order.products
   end  
   
-  def product_detail
-    products = self.products
-    
-    data = []
-
-    self.fabrication_order.rooms.each do |room|
-      rows = []
-
-      products.each do |prod|
-        tru = room.products.where("id=?",prod.id)[0]
-        unless tru.nil?
-          tru.product_sections.each do |sect|
-          
-            rows << sect.status
-          end
-        else   
-          prod.product_sections.each do |sect|
-          
-            rows << ""
-          end
-        end    
+  def products_group
+    arr_products = []
+    self.products.select("name").group("products.name").each do |prod|
+      sect_count = 0
+      max_col = 0
+      self.products.where("products.name = ?",prod.name).each do |prod2|    
+        ind_count = prod2.product_sections.count  
+        sect_count += ind_count
+        max_col = ind_count if ind_count > max_col
       end
-          
-      data << [room.name] + rows
+      arr_products << {:name => prod.name, :sect_count => sect_count, :max_col => max_col}  
     end  
+    return arr_products
+  end  
+  
+  def product_detail
+    products = []
+    data = []
+    total_col = 0
     
-    return data 
+    self.fabrication_order.rooms.each_with_index do |room, idx|
+      rows = [room.name]
+
+      self.products_group.each do |arr_prod|
+        room.products.each do |prod|
+          if arr_prod[:name] == prod.name
+            tru_sect_count = prod.product_sections.count 
+            rows += prod.product_sections.collect {|sect| "#{sect.status}-#{sect.product.name}"}
+                  
+          end    
+
+        end  
+             
+      end  
+      
+      data << rows
+    end
+    
+    return data   
   end  
   
 end
