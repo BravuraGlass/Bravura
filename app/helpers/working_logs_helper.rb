3afbc7953868b6c61c21ad085a74ef86008347cd
Aditya Agustyana <aditya@prawirasoft.com>
2017-11-07 16:09:09 +0800

    fixed hours select

--- app/helpers/working_logs_helper.rb
+++ app/helpers/working_logs_helper.rb
@@ -7,5 +7,29 @@ module WorkingLogsHelper
 
     format("%02d:%02d:%02d", hours, minutes, seconds) #=> "01:00:00
 
-  end 
+  end
+  
+  def hours_opts(therange)
+    rs = []
+    therange.to_a.each do |num|
+      if num.to_s.size == 1
+        rs << ["0#{num}",num]
+      else
+        rs << [num,num]
+      end    
+    end  
+    return rs
+  end  
+  
+  def bhours
+    hours_opts(0..23)
+  end
+  
+  def bminutes  
+    hours_opts(0..59)  
+  end
+  
+  def bseconds
+    bminutes
+  end       
 end
