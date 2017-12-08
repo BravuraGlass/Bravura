65f1c26115bf7a866baad505c863b37273eae541
Aditya Agustyana <aditya.jamop@gmail.com>
2017-12-08 21:00:43 +0800

    fixed map url on sidebar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(email: 'admin@admin.com',password: 'admin',first_name: 'Kevin', last_name: 'Admin',phonenumber: '555 555 5555',address: 'admin address',type_of_user: '0', always_access: true)
-User.create(email: 'admin3@admin.com',password: 'admin',first_name: 'Admin', last_name: 'Bravura3',phonenumber: '555 555 5555',address: 'admin address',type_of_user: '0', always_access: true)
-User.create(email: 'employee@employee.com',password: 'employee',first_name: 'Employee', last_name: 'Bravura',phonenumber: '555 555 5555',address: 'admin address',type_of_user: '1', always_access: false)
+user1 = User.create(email: 'admin@admin.com',password: 'admin',first_name: 'Kevin', last_name: 'Admin',phonenumber: '555 555 5555',address: 'admin address',type_of_user: '0', always_access: true)
+user2 = User.create(email: 'admin3@admin.com',password: 'admin',first_name: 'Admin', last_name: 'Bravura3',phonenumber: '555 555 5555',address: 'admin address',type_of_user: '0', always_access: true)
+user3 = User.create(email: 'employee@employee.com',password: 'employee',first_name: 'Employee', last_name: 'Bravura',phonenumber: '555 555 5555',address: 'admin address',type_of_user: '1', always_access: false)
 
 Status.create(category: "jobs",name: "Estimate", order: 1)
 Status.create(category: "jobs",name: "Proposal", order: 2)
@@ -42,15 +42,13 @@ Customer.create(contact_firstname: "Yukihiro", contact_lastname: "Mastsumoto", e
 time_now = Time.now
 start = (time_now - 1.week).to_i
 
-Location.create(user_id: 1, latitude: 40.7233, longitude: -74.003, created_at: Time.at(rand(time_now.to_i - start)) + start)
-Location.create(user_id: 2, latitude: 40.852, longitude: -74.0753, created_at: Time.at(rand(time_now.to_i - start)) + start)
-Location.create(user_id: 3, latitude: 40.7233, longitude: -74.003, created_at: Time.at(rand(time_now.to_i - start)) + start)
-Location.create(user_id: 1, latitude: 40.6488, longitude: -73.9464, created_at: Time.at(rand(time_now.to_i - start)) + start)
-Location.create(user_id: 2, latitude: 40.6937, longitude: -73.988, created_at: Time.at(rand(time_now.to_i - start)) + start)
-Location.create(user_id: 3, latitude: 40.6715, longitude: -73.9476, created_at: Time.at(rand(time_now.to_i - start)) + start)
-Location.create(user_id: 1, latitude: 40.7188, longitude: -73.9901, created_at: Time.at(rand(time_now.to_i - start)) + start)
-Location.create(user_id: 2, latitude: 40.6131, longitude: -73.963, created_at: Time.at(rand(time_now.to_i - start)) + start)
-Location.create(user_id: 3, latitude: 40.7282, longitude: -73.7949, created_at: Time.at(rand(time_now.to_i - start)) + start)
+Location.create(user: user1 , latitude: 40.7233, longitude: -74.003, created_at: Time.at(rand(time_now.to_i - start)) + start)
+Location.create(user: user2, latitude: 40.852, longitude: -74.0753, created_at: Time.at(rand(time_now.to_i - start)) + start)
+Location.create(user: user3, latitude: 40.7233, longitude: -74.003, created_at: Time.at(rand(time_now.to_i - start)) + start)
+Location.create(user: user1, latitude: 40.6488, longitude: -73.9464, created_at: Time.at(rand(time_now.to_i - start)) + start)
+Location.create(user: user2, latitude: 40.6937, longitude: -73.988, created_at: Time.at(rand(time_now.to_i - start)) + start)
+Location.create(user: user3, latitude: 40.6715, longitude: -73.9476, created_at: Time.at(rand(time_now.to_i - start)) + start)
+
 
 Employee.create(first_name: "Edison", last_name: "Cavani", email_address: "edison@psg.com")
 Employee.create(first_name: "Leroy", last_name: "Sane", email_address: "leroy@mancity.com")
