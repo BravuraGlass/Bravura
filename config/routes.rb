6e431913981dbdab7377e6a6dc33db523c8e6b16
Aditya Agustyana <aditya.jamop@gmail.com>
2018-01-27 17:28:20 +0800

    various changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   resources :notifications, only: [:create]
 
-  get 'locations/index'
+  #get 'locations/index'
 
   resources :working_logs, only: :index do
     collection do
